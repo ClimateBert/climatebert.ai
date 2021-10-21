3be97d0c1416ebe1c7b86ff17713a140945a9d74
Andreas Thomas <dev@chronark.com>
2021-10-21 23:40:44 +0200

    feat(navbar): set language model link to huggingface

---
+++ app/core/components/navbar.tsx
@@ -0,0 +1,104 @@
+import { MenuIcon, XIcon } from "@heroicons/react/outline"
+
+import { Disclosure } from "@headlessui/react"
+import { Link } from "blitz"
+import { Logo } from "app/core/components/logo"
+import React from "react"
+import cn from "classnames"
+import { useRouter } from "next/router"
+
+export const Navbar: React.FC = (): JSX.Element => {
+  const router = useRouter()
+
+  const pages: { name: string; href: string }[] = [
+    {
+      name: "Analyzer",
+      href: "/analyzer",
+    },
+    {
+      name: "Language Model",
+      href: "/language-model",
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+    {
+      name: "Authors",
+      href: "/authors",
+    },
+  ]
+
+  return (
+    <Disclosure as="nav" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+              <div className="flex justify-between w-full">
+                <Link href="/">
+                  <a className="flex items-center flex-shrink-0">
+                    <Logo />
+                    <h2 className="block p-2 text-xl font-medium transition duration-500 ease-in-out transform cursor-pointer text-coolGray-900 hover:text-coolcoolGray-500 lg:text-x lg:mr-8">
+                      ClimateBert
+                    </h2>
+                  </a>
+                </Link>
+                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                  {pages.map((page) => (
+                    <Link key={page.name} href={page.href}>
+                      <a
+                        className={cn(
+                          "inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2 ",
+                          {
+                            "border-rose-500 text-coolGray-900": router.pathname === page.href,
+                            "border-transparent text-coolGray-500 hover:border-coolGray-300 border-b-2 hover:text-coolGray-700":
+                              router.pathname !== page.href,
+                          }
+                        )}
+                      >
+                        {page.name}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex items-center -mr-2 sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-coolGray-400 hover:text-coolGray-500 hover:bg-coolGray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-500">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block w-6 h-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block w-6 h-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="pt-2 pb-3 space-y-1">
+              {/* Current: "bg-rose-50 border-rose-500 text-rose-700", Default: "border-transparent text-coolGray-500 hover:bg-coolGray-50 hover:border-coolGray-300 hover:text-coolGray-700" */}
+
+              {pages.map((page) => (
+                <Disclosure.Button
+                  key={page.name}
+                  as="a"
+                  href={page.href}
+                  className={cn("block py-2 pl-3 pr-4 text-base font-medium  border-l-4   ", {
+                    "border-rose-500 text-rose-700 bg-rose-50": router.pathname === page.href,
+                    "text-coolGray-500 border-transparent": router.pathname !== page.href,
+                  })}
+                >
+                  {page.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}

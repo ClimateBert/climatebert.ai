c0112e210cbd5a15be7ad3dc5de1a21be493c353
Andreas Thomas <dev@chronark.com>
2021-10-22 19:45:47 +0200

    feat: footer and hero

--- app/pages/analyzer.tsx
+++ app/pages/analyzer.tsx
@@ -10,7 +10,7 @@ export default function Analyzer() {
           <h2 className="inline text-3xl font-extrabold tracking-tight text-gray-900 sm:block sm:text-4xl">
             Coming soon
           </h2>
-          <p className="inline text-3xl font-extrabold tracking-tight text-rose-600 sm:block sm:text-4xl ">
+          <p className="inline text-3xl font-extrabold tracking-tight text-blue-600 sm:block sm:text-4xl ">
             Get notified as soon as we launch!
           </p>
           <form className="mt-8 sm:flex">
@@ -23,13 +23,13 @@ export default function Analyzer() {
               type="email"
               autoComplete="email"
               required
-              className="w-full px-5 py-3 placeholder-gray-500 border-gray-300 rounded-sm focus:ring-rose-500 focus:border-rose-500 sm:max-w-xs"
+              className="w-full px-5 py-3 placeholder-gray-500 border-gray-300 rounded-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs"
               placeholder="Enter your email"
             />
             <div className="mt-3 rounded-sm shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
               <button
                 type="submit"
-                className="flex items-center justify-center w-full px-10 py-3 text-base font-medium text-white bg-rose-600 border border-transparent rounded-sm hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+                className="flex items-center justify-center w-full px-10 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Notify me
               </button>
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,29 +1,79 @@
-import Link from "next/link"
 import { Navbar } from "app/core/components/navbar"
-import { CubeTransparentIcon, VariableIcon, LightningBoltIcon } from "@heroicons/react/outline"
+import {
+  CubeTransparentIcon,
+  VariableIcon,
+  LightningBoltIcon,
+  MailIcon,
+} from "@heroicons/react/outline"
 import { Section } from "app/core/components/section"
+import Footer from "app/core/components/footer"
+import image from "public/hero.png"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [scroll, setScroll] = useState(typeof window !== "undefined" ? window.scrollY : 0)
+  useEffect(() => {
+    const cb = () => setScroll(window.scrollY)
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", cb)
+    }
+    return () => {
+      window.removeEventListener("scroll", cb)
+    }
+  })
+
   return (
     <>
-      <div className="h-screen ">
-        <Navbar />
-        <div className="flex flex-col justify-center h-full pt-16 -mt-16">
-          <div className="flex justify-center text-center md:text-left">
-            <div className="flex items-center justify-start text-coolGray-50">
-              <div className="flex flex-col items-center space-y-20">
-                <h1 className="flex flex-col gap-3 font-black text-center text-black text-7xl sm:text-9xl">
-                  <span>Analyze.</span>
-                  <span>Reflect.</span>
-                  <span>Engage.</span>
-                </h1>
-                <Link href="/analyzer">
-                  <a className="inline-flex justify-center px-20 py-3 text-2xl font-semibold text-white duration-500 bg-black border-2 border-black hover:bg-opacity-80 hover:text-black hover:bg-white">
-                    Start Now
-                  </a>
-                </Link>
-                <h2 className="p-2 text-xl text-coolGray-600">
-                  AI powered climate-related corporate disclosure analytics
-                </h2>
+      <div className="h-screen">
+        <div className="relative w-full h-full ">
+          <div
+            className="absolute w-screen h-screen"
+            style={{
+              backgroundImage: "url('/hero.png')",
+              backgroundAttachment: "fixed",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              opacity: `${Math.min(10 + scrollY / 5, 100)}%`,
+            }}
+          ></div>
+          <div className="relative z-10 h-screen">
+            <Navbar />
+            <div className="relative flex flex-col justify-center h-full pt-16 -mt-16">
+              <div className="flex justify-center text-center md:text-left">
+                <div className="flex items-center justify-start text-coolGray-50">
+                  <div className="flex flex-col items-center space-y-16">
+                    <h1 className="flex flex-col gap-3 font-black text-center text-black text-7xl sm:text-9xl">
+                      <span>Analyze.</span>
+                      <span>Reflect.</span>
+                      <span>Engage.</span>
+                    </h1>
+                    <div className="text-black">
+                      <label htmlFor="email" className="block text-sm text-center uppercase">
+                        Join the waitlist to get early access
+                      </label>
+                      <div className="flex flex-col items-center justify-center gap-4 mt-3 md:flex-row">
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                          className="h-12 px-4 duration-700 border border-gray-500 rounded-sm shadow form-input hover:border-black focus:border-black hover:shadow-2xl focus:outline-none"
+                          placeholder="Enter your email"
+                        />
+                        <button
+                          type="button"
+                          className="h-12 px-4 font-medium text-white uppercase duration-700 bg-black border border-gray-500 rounded-sm shadow hover:bg-white hover:text-black hover:border-black focus:outline-none hover:shadow-2xl"
+                        >
+                          <span>Get early access</span>
+                        </button>
+                      </div>
+                    </div>
+                    <h2 className="p-2 text-xl text-coolGray-600">
+                      AI powered climate-related corporate disclosure analytics
+                    </h2>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -53,196 +103,7 @@ export default function Home() {
           />
         </div>
       </main>
+      <Footer />
     </>
-    // <div className="bg-coolGray-100">
-    //   <section className="h-screen bg-black">
-    //     <Background image={headerImage}>
-    //       <div className="bg-gradient-to-tr from-transparent via-transparent to-black">
-    //         <header className="absolute inset-x-0 hidden text-xl font-light md:block text-coolGray-100">
-    //           <div className="container flex items-center justify-between px-4 py-8 mx-auto">
-    //             <Link href="/">
-    //               <a className="flex items-center gap-2 duration-500 border-b-2 border-transparent whitespace-nowrap hover:border-coolGray-200 hover:text-white">
-    //                 <Logo />
-    //                 <span className="font-bold">ClimateBERT</span>
-    //               </a>
-    //             </Link>
-
-    //             <nav className="flex items-center justify-center gap-4 text-xl duration-500 lg:gap-8 xl:gap-16 lg:text-2xl text-coolGray-100 ">
-    //               <Link href="/analyzer">
-    //                 <a className="border-b-2 border-transparent whitespace-nowrap hover:border-coolGray-200 hover:text-white">
-    //                   Analyzer
-    //                 </a>
-    //               </Link>
-    //               <a
-    //                 href="https://huggingface.co/climatebert/distilroberta-base-climate"
-    //                 target="_blank"
-    //                 rel="noreferrer"
-    //                 className="border-b-2 border-transparent whitespace-nowrap hover:border-coolGray-200 hover:text-white"
-    //               >
-    //                 Language Model
-    //               </a>
-    //               <Link href="/about">
-    //                 <a className="border-b-2 border-transparent whitespace-nowrap hover:border-coolGray-200 hover:text-white">
-    //                   About
-    //                 </a>
-    //               </Link>
-    //               <Link href="/authors">
-    //                 <a className="border-b-2 border-transparent whitespace-nowrap hover:border-coolGray-200 hover:text-white">
-    //                   Authors
-    //                 </a>
-    //               </Link>
-    //               <Link href="https://github.com/ClimateBert/climatebert.ai">
-    //                 <a className="inline-flex justify-center border-b-2 border-transparent whitespace-nowrap hover:border-coolGray-200 hover:text-white">
-    //                   <svg
-    //                     className="w-6 h-6 fill-current"
-    //                     xmlns="http://www.w3.org/2000/svg"
-    //                     viewBox="0 0 512 512"
-    //                   >
-    //                     <path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9 1.4.3 2.6.4 3.8.4 8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1-8.4 1.9-15.9 2.7-22.6 2.7-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1 10.5 0 20-3.4 25.6-6 2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8 0 0 1.6-.5 5-.5 8.1 0 26.4 3.1 56.6 24.1 17.9-5.1 37-7.6 56.1-7.7 19 .1 38.2 2.6 56.1 7.7 30.2-21 48.5-24.1 56.6-24.1 3.4 0 5 .5 5 .5 12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5 1.2 0 2.6-.1 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z" />
-    //                   </svg>
-    //                 </a>
-    //               </Link>
-    //             </nav>
-    //           </div>
-    //         </header>
-
-    //         <div className="flex flex-col justify-center h-screen">
-    //           <div className="flex justify-center text-center md:text-left">
-    //             <div className="flex items-center justify-start text-coolGray-50">
-    //               <div className="flex flex-col items-center">
-    //                 <h2 className="p-2 text-xl shadow-lg bg-opacity-20 text-coolGray-300">
-    //                   AI powered climate-related corporate disclosure analytics
-    //                 </h2>
-    //                 <h1 className="flex flex-col gap-3 mt-2 font-black text-center text-white text-7xl sm:text-9xl">
-    //                   <span>Analyze.</span>
-    //                   <span>Reflect.</span>
-    //                   <span>Engage.</span>
-    //                 </h1>
-    //                 <div className="mt-20">
-    //                   <Link href="/analyzer">
-    //                     <a className="inline-flex justify-center py-3 text-2xl font-semibold text-black duration-500 border-2 w-72 hover:bg-black hover:bg-opacity-80 hover:text-coolGray-100 border-coolGray-200 bg-coolGray-50">
-    //                       Start Now
-    //                     </a>
-    //                   </Link>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </Background>
-    //   </section>
-    //   <section />
-    //   <section>
-    //     <Background image={iceland}>
-    //       <div className="bg-gradient-to-tr bg-opacity-70 bg-coolGray-100 from-coolGray-100 via-coolGray-100 to-transparent">
-    //         <div className="container mx-auto">
-    //           <div className="p-12 xl:w-1/2 md:py-48 ">
-    //             <h2 className="text-4xl font-bold text-coolGray-900">Transparency</h2>
-    //             <p className="mt-4 text-lg font-light text-coolGray-800">
-    //               We enable companies’ stakeholders to assess all kinds of climate-related corporate
-    //               disclosures in an efficient and scalable way. This increases transparency
-    //               massively.
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </Background>
-    //   </section>
-    //   <section>
-    //     <Background image={tech}>
-    //       <div className="bg-opacity-70 bg-coolGray-900 bg-gradient-to-tl from-black via-coolGray-900 to-transparent">
-    //         <div className="container flex flex-row-reverse w-full mx-auto">
-    //           <div className="p-12 xl:w-1/2 md:py-48 md:text-right">
-    //             <h2 className="text-4xl font-bold text-coolGray-100">
-    //               State-of-the-art AI technology
-    //             </h2>
-    //             <p className="mt-4 text-lg font-light t text-coolGray-200">
-    //               We apply state-of-the-art AI technology to assess climate-related corporate
-    //               disclosures. ClimateBERT has been trained on thousands of climate-related texts,
-    //               making it a powerful tool to assist you.
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </Background>
-    //   </section>
-    //   <section>
-    //     <Background image={iceland2}>
-    //       <div className="bg-gradient-to-tr bg-opacity-70 bg-coolGray-100 from-coolGray-100 via-coolGray-100 to-transparent">
-    //         <div className="container mx-auto">
-    //           <div className="p-12 xl:w-1/2 md:py-48">
-    //             <h2 className="text-4xl font-bold text-coolGray-900">Analyze, conclude, engage</h2>
-    //             <p className="mt-4 text-lg font-light text-coolGray-800">
-    //               Our service gives you the opportunity to have an impact. Start analyzing, get
-    //               insights and draw your own conclusions. Let’s protect our planet together.
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </Background>
-    //   </section>
-    //   <footer className="bg-black">
-    //     <Background image={footer}>
-    //       <div className="p-16 bg-gradient-to-tr from-black via-transparent to-black">
-    //         <div className="container px-4 py-12 mx-auto sm:px-6 lg:px-8">
-    //           <div className="grid w-40 grid-cols-1 gap-4 mx-auto sm:w-full sm:grid-cols-3 lg:gap-24 sm:gap-8 sm md:gap-16 xl:gap-32 text-coolGray-200">
-    //             <Link href="https://ethz.ch">
-    //               <a className="hover:text-white">
-    //                 <ETHZLogo />
-    //               </a>
-    //             </Link>
-    //             <Link href="https://fau.de">
-    //               <a className="hover:text-white">
-    //                 <FAULogo />
-    //               </a>
-    //             </Link>
-    //             <Link href="https://uzh.ch">
-    //               <a className="hover:text-white">
-    //                 <UZHLogo />
-    //               </a>
-    //             </Link>
-    //           </div>
-    //         </div>
-
-    //         <div className="container px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-    //           <div className="flex justify-center">
-    //             <nav className="flex flex-col space-y-4 sm:flex-row sm:space-x-8 sm:space-y-0">
-    //               <Link href="/analyzer">
-    //                 <a className="text-coolGray-300 whitespace-nowrap hover:text-coolGray-100">
-    //                   Analyzer
-    //                 </a>
-    //               </Link>
-    //               <Link href="/language-model">
-    //                 <a className="text-coolGray-300 whitespace-nowrap hover:text-coolGray-100">
-    //                   Language Model
-    //                 </a>
-    //               </Link>
-    //               <Link href="/about">
-    //                 <a className="text-coolGray-300 whitespace-nowrap hover:text-coolGray-100">
-    //                   About
-    //                 </a>
-    //               </Link>
-    //               <Link href="/authors">
-    //                 <a className="text-coolGray-300 whitespace-nowrap hover:text-coolGray-100">
-    //                   Authors
-    //                 </a>
-    //               </Link>
-    //               <Link href="mailto:info@climatebert.ai">
-    //                 <a className="text-coolGray-300 whitespace-nowrap hover:text-coolGray-100">
-    //                   Contact
-    //                 </a>
-    //               </Link>
-    //             </nav>
-    //           </div>
-
-    //           <p className="mt-8 text-center text-coolGray-400">
-    //             © {new Date().getFullYear()} All rights reserved.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </Background>
-    //   </footer>
-    // </div>
   )
 }

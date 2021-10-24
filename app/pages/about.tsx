5a8ec353937b73e6b6bc6a4686c9ee47256bc85b
webersni <nicolas.webersinke@outlook.com>
2021-10-24 16:22:36 +0200

    Add content for about page

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -9,68 +9,66 @@ export default function About() {
         <div className="mx-auto text-lg max-w-prose">
           <h1>
             <span className="block text-base font-semibold tracking-wide text-center text-transparent uppercase bg-clip-text bg-gradient-to-r from-primary-600 to-secondary-400">
-              Introducing
+              About
             </span>
             <span className="block mt-2 text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl">
-              JavaScript for Beginners
+              ClimateBert
             </span>
           </h1>
-          <p className="mt-8 text-xl leading-8 text-gray-500">
-            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi,
-            nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at
-            in viverra scelerisque eget. Eleifend egestas fringilla sapien.
+          <p className="mt-8 prose prose-lg text-gray-500 prose-blue">
+            <strong>Climatebert.ai</strong> is a joint research project of{" "}
+            <strong>Julia Anna Bingler</strong> from ETH Zürich, <strong>Mathias Kraus</strong> and{" "}
+            <strong>Nicolas Webersinke</strong> from FAU Erlangen-Nürnberg, and{" "}
+            <strong>Markus Leippold</strong> from University of Zürich. For more information about
+            the authors and their background, see <a href="/authors">Authors</a>.
           </p>
         </div>
         <div className="mx-auto mt-6 prose prose-lg text-gray-500 prose-blue">
           <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed{" "}
-            <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id
-            dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat
-            velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.{" "}
-            <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
+            Our project started in 2019 with the overall aim to make climate-related unstructured
+            textual information from various sources available for research, policy-making,
+            financial supervisory authorities, and financial analysts. As a first step, we trained
+            the model to analyse climate-related disclosures of companies (see our working paper on{" "}
+            <a href="https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3796152">SSRN</a>). Ever
+            since then, ClimateBERT has constantly evolved.
           </p>
-          <ul role="list">
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
+          <h2>Language Model</h2>
           <p>
-            Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit
-            viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum
-            ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
-            elit interdum dignissim.
-          </p>
-          <h2>From beginner to expert in 30 days</h2>
-          <p>
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu
-            ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh.
-            Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus
-            ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.
-          </p>
-          <blockquote>
-            <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed
-              consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.
-            </p>
-          </blockquote>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-            semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus
-            viverra tellus varius sit neque erat velit.
-          </p>
-
-          <h2>Everything you need to get up and running</h2>
-          <p>
-            Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam
-            varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique.
-            Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget
-            dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras
-            fermentum convallis quam.
+            ClimateBERT is the name of our transformer-based language model adapted for use for
+            climate-related text and has been fine-tuned on various downstream tasks.
           </p>
           <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-            semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus
-            viverra tellus varius sit neque erat velit.
+            See <a href="/language-model">Language Model</a> for more information on our language
+            model.
+          </p>
+          <h2>Downstream Tasks</h2>
+          <p>
+            So far, ClimateBERT has been fine-tuned on six downstream tasks. It is able to
+            <ol role="list">
+              <li>detect climate content in text files,</li>
+              <li>assess the sentiment of this content,</li>
+              <li>fact-check climate-related claims,</li>
+              <li>
+                assign a climate disclosure category to the climate-related content (based on the
+                four categories of the recommendations of the Task Force on Climate-related
+                Financial Disclosures (TCFD), see XYZ)
+              </li>
+              <li>
+                identify whether climate-related content is a commitment for climate action, and
+              </li>
+              <li>
+                to assess whether climate-related content is rather specific or unspecific
+                boilerplate language
+              </li>
+            </ol>
+          </p>
+          <p>
+            The additional downstream tasks that ClimateBERT has been trained on since our first
+            steps could serve various use cases. For example, it could aid financial supervisors to
+            assess the state of corporate climate risk disclosures. Or it could support governments
+            in their recent activities to detect corporate greenwashing activities. Financial
+            analysts might use ClimateBERT to identify the climate risk and opportunities that a
+            company
           </p>
         </div>
       </div>

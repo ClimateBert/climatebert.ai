f532a0b34040ed55064bc22723bbc84814f92c70
webersni <nicolas.webersinke@outlook.com>
2021-10-24 11:07:01 +0200

    Add authors' descriptions

--- app/pages/authors.tsx
+++ app/pages/authors.tsx
@@ -71,10 +71,7 @@ const AuthorsPage: NextPage<AuthorsPageProps> = ({ authors }) => {
               <h2 className="text-2xl font-bold text-gray-800 ">{author.name}</h2>
               <h3 className="mt-2 italic text-gray-600">{author.title}</h3>
               <p className="mt-6 text-gray-500 dark:text-gray-400 lg:max-w-md">
-                Proin luctus malesuada lobortis. Curabitur sit amet enim odio. Morbi sit amet nisl
-                commodo, posuere eros id, pulvinar diam. Donec eget tortor nec metus ornare
-                eleifend. Donec non mauris quis est maximus gravida. Aenean sit amet fermentum
-                velit. In dapibus magna sed odio fringilla aliquet.
+                {author.description}
               </p>
             </div>
 
@@ -121,7 +118,8 @@ export const getStaticProps: GetStaticProps<AuthorsPageProps> = () => {
           name: "Julia Anna Bingler",
           title: "Doctoral Researcher, ETH Zürich",
           social: { linkedIn: "https://ch.linkedin.com/in/jubingler" },
-          description: "abc",
+          description:
+            "Julia Anna Bingler is Doctoral Researcher at the Center of Economic Research at ETH Zurich. Her research focuses on climate transition risk metrics and the decision-relevance of TCFD-related disclosures. Prior to joining ETH in 2019, she has been working on sustainable finance and climate policy in various positions, both inside and outside academia. She regularly advises stakeholders as an expert on corporate climate-related risk analysis and disclosures, on national and international climate policy processes, and participated in various UNFCCC climate conferences as an observer on finance-related topics.",
 
           department:
             "https://resec.ethz.ch/people/person-detail.MjQ5OTkw.TGlzdC8zMDI2LC0yMDk5NTM5OTY=.html",
@@ -135,7 +133,8 @@ export const getStaticProps: GetStaticProps<AuthorsPageProps> = () => {
             linkedIn: " https://de.linkedin.com/in/mathias-kraus-639431113",
           },
 
-          description: "abc",
+          description:
+            "Mathias Kraus holds the assistant professorship of Data Analytics at the Institute of Information Systems at FAU Erlangen-Nuremberg. Before joining FAU, he worked as a research assistant at the University of Freiburg in 2017 and as a PhD student at ETH Zurich from 2018 to 2020, where he successfully completed his PhD on “Deep Learning in Business Analytics: Methods and Applications” in October 2020. His work has been published in top-tier computer science conferences (e.g., SIGKDD), as well as in prestigious journals in the field of operations research (e.g., European Journal of Operational Research).",
           department: "https://www.data-analytics.rw.fau.eu/about-us/",
           image: "/authors/mathias_kraus.jpg",
           uni: "fau",
@@ -146,19 +145,21 @@ export const getStaticProps: GetStaticProps<AuthorsPageProps> = () => {
           social: {
             linkedIn: "https://ch.linkedin.com/in/markus-leippold-578bb95",
           },
-          description: "abc",
+          description:
+            "Markus Leippold is a professor at the University of Zurich, where he holds the Chair in Financial Engineering. Before joining the University of Zurich in 2009, Markus was an associate professor in quantitative finance at Imperial College Business School, London. In 2005, he was a visiting professor at the Federal Reserve Bank in New York. Markus obtained his Ph.D. in economics from the University of St. Gallen, Switzerland, in 1999, and after some years in the financial industry, he started as an assistant professor at the University of Zurich in 2002. Markus published in top academic journals like the Journal of Financial Economics, Review of Financial Studies, Management Science, and Annals of Statistics. Most recently, Markus spent a research semester at Google, digging deeper into research on natural language processing.",
           department: "https://www.bf.uzh.ch/en/persons/leippold-markus/detail",
           image: "/authors/markus_leippold.jpg",
           uni: "uzh",
         },
         {
           name: "Nicolas Webersinke",
-          title: "Research Associate, University of Erlangen-Nuremberg",
+          title: "Doctoral Researcher, University of Erlangen-Nuremberg",
           social: {
             linkedIn: "https://de.linkedin.com/in/nicolas-webersinke-6b45a6150/",
             github: "https://github.com/webersni",
           },
-          description: "abc",
+          description:
+            "Nicolas Webersinke is Doctoral Researcher at the Chair of Finance and Banking at FAU Erlangen-Nuremberg. His research focuses thematically on sustainable finance and methodologically on the intersection of finance and data science. In addition to his PhD studies, he is pursuing a second master's degree in International Information Systems, majoring in data science and software engineering.",
           department: "https://www.lfb.rw.fau.de/person/nicolas-webersinke/",
           image: "/authors/nicolas_webersinke.jpg",
           uni: "fau",

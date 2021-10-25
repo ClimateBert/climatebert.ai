40913201c3ec0bbabe10dc8eb9265df7da72277a
webersni <nicolas.webersinke@outlook.com>
2021-10-25 21:59:31 +0200

    Update exact number of model parameters

--- app/core/components/stats.tsx
+++ app/core/components/stats.tsx
@@ -8,7 +8,7 @@ export const Stats: React.FC = (): JSX.Element => {
           <div className="flex flex-col p-6 text-center border-t border-b border-gray-100 sm:border-0 sm:border-l sm:border-r">
             <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">Parameters</dt>
             <dd className="order-1 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-secondary-400">
-              82,000,000
+              82,350,916
             </dd>
           </div>
           <div className="flex flex-col p-6 text-center border-t border-gray-100 sm:border-0 sm:border-l">
